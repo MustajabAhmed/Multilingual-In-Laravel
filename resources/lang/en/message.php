07040e2cd6328fd6c5f4787dbdfff8ca8035c8e7
MustajabAhmed <mustajabahmed02@gmail.com>
2023-07-05 09:42:02 +0500

    MultiLingual in laravel

---
+++ resources/lang/en/message.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    'welcome' => 'Welcome',
+    'hello' => 'Hello',
+];
---
+++ routes/web.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Mcamara\LaravelLocalization\Facades\LaravelLocalization;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::group(['prefix' => LaravelLocalization::setLocale()], function () {
+    Route::get('/', function () {
+        return view('welcome');
+    });
+});
